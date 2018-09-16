b53920ba007f6547bf06b06309c5d3e9e33f0d0d
Jussi <jussi@lingvi.st>
2018-09-16 10:21:27 +0200

    return the calculation instead of printing it; chg output to go via logger instead

--- khi2.py
+++ khi2.py
@@ -1,31 +1,31 @@
 import numpy as np
+from logger import logger
 import math
 
-def khi2(crosstab):
+def khi2(crosstab, loglevel=False):
     d = crosstab.shape
     if (len (d)  > 2):
         raise("Wrong format for this function")
     rows=d[0]  # unnecessary, but clear
     cols=d[1]  # unnecessary, but clear
-    print("Degrees of freedom: ",(rows-1)*(cols-1))
-    print("Marginal sums:")
+    streng = "Degrees of freedom: " + str((rows-1)*(cols-1))
+    streng += "Marginal sums:\n"
     expecteds=margins3(crosstab)
-    print(expecteds)
+    streng += str(expecteds)
     for k in range(rows):
         for l in range(cols):
             expecteds[k,l]=expecteds[k,cols]*expecteds[rows,l]/expecteds[rows,cols]
-    print("Expected values:")
-    print(expecteds)
+    streng += "\nExpected values:\n"
+    streng += str(expecteds) + "\n"
     sum = 0
     deviation=np.zeros((rows+1,cols+1))
     for k in range(rows):
         for l in range(cols):
             deviation[k,l]=((expecteds[k,l]-crosstab[k,l]))
             sum += deviation[k,l]**2/expecteds[k,l]
-    print(crosstab)
-    print(deviation)
-    print(sum)
-
+    streng += str(crosstab) + "\n" + str(deviation) + "\n" + str(sum) + "\n"
+    logger(streng, loglevel)
+    return sum
 
 
 def margins3 (a):
